10af927887f2214d996f00bc997ada063af9f279
Ostanin <OstaninKY@gmail.com>
2026-08-03 18:23:15 +0300

    :pagination

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -1,3 +1,6 @@
+import csv
+
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from django.urls import reverse
 
@@ -9,9 +12,30 @@ def index(request):
 def bus_stations(request):
     # получите текущую страницу и передайте ее в контекст
     # также передайте в контекст список станций на странице
+    stations_list = []
+
+    with open('data-398-2018-08-30.csv', newline='', encoding='utf8') as csvfile:
+        reader = csv.DictReader(csvfile)
+
+        for row in reader:
+            station_info = {
+                'name': row['Name'] if 'Name' in row else row['name'],
+                'street': row['Street'],
+                'district': row['District']
+            }
+            stations_list.append(station_info)
+
+    page_number = request.GET.get('page', 1)
+
+    paginator = Paginator(stations_list, 10)
+
+    page_obj = paginator.get_page(page_number)
 
     context = {
-    #     'bus_stations': ...,
-    #     'page': ...,
+        # Передаем не весь список, а только объекты текущей страницы
+        'bus_stations': page_obj.object_list,
+
+        # Объект page удобен для вывода кнопок "Вперед", "Назад" и номеров страниц в шаблоне
+        'page': page_obj,
     }
     return render(request, 'stations/index.html', context)
