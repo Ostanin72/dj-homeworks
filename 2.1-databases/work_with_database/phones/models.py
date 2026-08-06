4048e2555a7b2bea302de6c4496e09e96d5fd021
Ostanin <OstaninKY@gmail.com>
2026-08-06 15:22:58 +0300

    :update vievs models settings

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -1,6 +1,20 @@
+from autoslug import AutoSlugField
 from django.db import models
 
 
 class Phone(models.Model):
     # TODO: Добавьте требуемые поля
-    pass
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    image = models.ImageField(
+        upload_to='images/',
+        null = True,
+        blank = True,
+    )
+    release_date = models.DateField()
+    lte_exists = models.BooleanField(default=False)
+    slug = AutoSlugField(populate_from='name', unique=True, editable=False)
+
+
+    def __str__(self):
+        return self.name
--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+
+from phones.models import Phone
 
 
 def index(request):
@@ -7,11 +9,27 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    phone_list = Phone.objects.all()
+    sort_param = request.GET.get('sort', '')
+    if sort_param == 'name':
+        phone_list = phone_list.order_by('name')
+    elif sort_param == 'min_price':
+        phone_list = phone_list.order_by('price')
+    elif sort_param == 'max_price':
+        phone_list = phone_list.order_by('-price')
+
+    context = {
+        'phones': phone_list
+    }
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    phone_product = get_object_or_404(Phone, slug=slug)
+
+    context = {
+        'phone': phone_product
+    }
+
     return render(request, template, context)
