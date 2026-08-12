ea4aad3875bfadf683b9a8c71ce39c760d0ad13c
Ostanin <OstaninKY@gmail.com>
2026-08-12 10:39:06 +0300

    :update vievs urls templates

--- 2.1-databases/models_list_displaying/books/views.py
+++ 2.1-databases/models_list_displaying/books/views.py
@@ -1,7 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from books.models import Book
+
+
+def index(request):
+    return redirect('books')
 
 def books_view(request):
     template = 'books/books_list.html'
-    context = {}
+
+    context = {
+        'books': Book.objects.all(),
+    }
+    return render(request, template, context)
+
+def books_view_date(request, pub_date):
+    template = 'books/book_date.html'
+    books_on_this_date = Book.objects.filter(
+        pub_date=pub_date
+    ).order_by('name')
+    current_book = books_on_this_date.first()
+    previous_book = Book.objects.filter(
+        pub_date__lt=current_book.pub_date
+    ).order_by('-pub_date').first()
+    next_book = Book.objects.filter(
+        pub_date__gt=current_book.pub_date
+    ).order_by('pub_date').first()
+
+    context = {
+        'books': books_on_this_date,
+        'current_book': current_book,
+        'previous_book': previous_book,
+        'next_book': next_book,
+    }
+
     return render(request, template, context)
--- 2.1-databases/models_list_displaying/main/urls.py
+++ 2.1-databases/models_list_displaying/main/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from books.views import books_view
+from books.views import books_view, index, books_view_date
+
 
 urlpatterns = [
-    path('', books_view, name='books'),
+    path('', index),
+    path('books/', books_view, name='books'),
+    path('books/<str:pub_date>/', books_view_date, name='book'),
     path('admin/', admin.site.urls),
 ]
