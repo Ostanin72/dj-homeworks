f1890de01038a44f794727d775e6a514d9e09d8b
Ostanin <OstaninKY@gmail.com>
2026-09-04 08:51:13 +0300

    update stocks_products

--- 3.2-crud/stocks_products/logistic/views.py
+++ 3.2-crud/stocks_products/logistic/views.py
@@ -1,3 +1,5 @@
+from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.viewsets import ModelViewSet
 
 from logistic.models import Product, Stock
@@ -8,9 +10,16 @@ class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     # при необходимости добавьте параметры фильтрации
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = ['title', 'description']
+    ordering_fields = ['id',]
+    pagination_class = LimitOffsetPagination
 
 
 class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
-    # при необходимости добавьте параметры фильтрации
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = ['products__title', 'products__description']
+    ordering_fields = ['id',]
+    pagination_class = LimitOffsetPagination
